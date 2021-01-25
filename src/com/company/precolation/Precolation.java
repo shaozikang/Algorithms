68d3d950720ae8ab14dc007d4fdad602b05fb96d
shaozikang <913667678@qq.com>
2021-01-25 14:40:51 +0800

    add hello

---
+++ src/com/company/precolation/Precolation.java
@@ -0,0 +1,8 @@
+package com.company.precolation;
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Precolation {
+}
